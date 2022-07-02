bc316bad6299ea4c8b72deae25ed0cc24ff8a68a
Bhimgouda <satvikp508@gmail.com>
2022-07-02 12:07:43 +0530

    initial function setup

--- app.js
+++ app.js
@@ -18,7 +18,7 @@
       "id": 2,
       "company": "Manage",
       "logo": "./images/manage.svg",
-      "new": true,
+      "new": false,
       "featured": true,
       "position": "Fullstack Developer",
       "role": "Fullstack",
@@ -48,7 +48,7 @@
       "id": 4,
       "company": "MyHome",
       "logo": "./images/myhome.svg",
-      "new": false,
+      "new": true,
       "featured": false,
       "position": "Junior Frontend Developer",
       "role": "Frontend",
@@ -78,8 +78,8 @@
       "id": 6,
       "company": "FaceIt",
       "logo": "./images/faceit.svg",
-      "new": false,
-      "featured": false,
+      "new": true,
+      "featured": true,
       "position": "Junior Backend Developer",
       "role": "Backend",
       "level": "Junior",
@@ -93,7 +93,7 @@
       "id": 7,
       "company": "Shortly",
       "logo": "./images/shortly.svg",
-      "new": false,
+      "new": true,
       "featured": false,
       "position": "Junior Developer",
       "role": "Frontend",
@@ -109,7 +109,7 @@
       "company": "Insure",
       "logo": "./images/insure.svg",
       "new": false,
-      "featured": false,
+      "featured": true,
       "position": "Junior Frontend Developer",
       "role": "Frontend",
       "level": "Junior",
@@ -139,7 +139,7 @@
       "company": "The Air Filter Company",
       "logo": "./images/the-air-filter-company.svg",
       "new": false,
-      "featured": false,
+      "featured": true,
       "position": "Front-end Dev",
       "role": "Frontend",
       "level": "Junior",
@@ -153,8 +153,153 @@
 
 jobData = JSON.parse(jobData);
 
-const skill = document.querySelector('.skill');
 
-skill.textContent = 'CSS';
+const jobBadges = document.querySelectorAll('.job-badge');
+const selectedArea = document.querySelector('.selected-badges');
+const jobSearch = document.querySelector('.job-search');
+const jobListing = document.querySelector('.job-listing');
+const clearFilterButton = document.querySelector('#clear-filters')
+
+/* Selecting Dummy job items */
+
+const dummyJob = document.querySelector('.job')
+let companyLogo = document.querySelector('.job__company-logo');
+let company = document.querySelector('.job__company-name');
+let position = document.querySelector('.job__role');
+let postedAt = document.querySelector('.was-listed');
+let contract = document.querySelector('.job__duration'); 
+let jobLocation = document.querySelector('.job__location');
+let newJobBadge = document.querySelector('.new-badge');
+let featuredJobBadge = document.querySelector('.featured-badge');
+let dummyJobBadges = document.querySelector('.job__badges');
+
+
+/* Onload with Given job Data */
+
+window.addEventListener('load',()=>{
+  dummyJob.style.display = 'none';
+  jobSearch.classList.add('job-search--hidden');
+  jobData.forEach((job,index)=>{
+    newJob = document.createElement('div');
+    newJob.classList.add('job');
+
+
+    /* Making changes to dummy job according to job data */
+    companyLogo.innerHTML = `<object data="${job.logo}" type="image/svg+xml"></object>`;
+    company.textContent = `${job.company}`;
+    position.textContent = `${job.position}`;
+    postedAt.textContent = `${job.postedAt}`;
+    contract.textContent = `${job.contract}`;
+    jobLocation.textContent = `${job.location}`;
+
+
+
+    /* Checking Featured and New Jobs */
+
+    if(!job.featured){
+      featuredJobBadge.style.display = 'none'
+    }
+    else{
+      featuredJobBadge.style.display = 'inline-block'
+    }
+    
+    if(!job.new){
+      newJobBadge.style.display = 'none'
+    }
+    else{
+      newJobBadge.style.display = 'inline-block'
+    }
+
+
+    /* Adding Job Badges */
+
+    dummyJobBadges.innerHTML = ``;
+    dummyJobBadges.innerHTML += `<span class="job-badge">${job.role}</span>`
+    if (job.tools){
+      job.tools.forEach(tool=>{
+        dummyJobBadges.innerHTML += `<span class="job-badge">${tool}</span>`
+      })
+    }
+    if(job.languages){
+      job.languages.forEach(language=>{
+        dummyJobBadges.innerHTML += `<span class="job-badge">${language}</span>`
+      })
+    }
+
+
+    /* Copying its html to newJob */
+    newJob.innerHTML = dummyJob.innerHTML
+
+
+  jobListing.appendChild(newJob);
+  })
+})
+
+
+let searchFilterList = [];
+
+  jobListing.addEventListener('click', (e)=>{
+    if (e.target.classList.contains('job-badge')){
+      jobSearch.classList.remove('job-search--hidden');
+      let badgeText = e.target.textContent;
+      if(!selectedArea.innerHTML.includes(badgeText)){
+        searchFilterList.push(badgeText);
+        let newBadge = document.createElement('span');
+        newBadge.classList.add('selected-badge');
+        newBadge.innerHTML = `${badgeText}
+        <span class="remove-icon remove1">
+          <svg class="remove2" xmlns="http://www.w3.org/2000/svg" width="14" height="14">
+            <path class = 'remove3'
+              fill="#FFF"
+              fill-rule="evenodd"
+              d="M11.314 0l2.121 2.121-4.596 4.596 4.596 4.597-2.121 2.121-4.597-4.596-4.596 4.596L0 11.314l4.596-4.597L0 2.121 2.121 0l4.596 4.596L11.314 0z"
+            />
+          </svg>
+        </span>`
+        selectedArea.appendChild(newBadge);
+      }
+    }
+    console.log(searchFilterList)
+  })
+
+
+
+/* Remove all filters button */
+
+clearFilterButton.addEventListener('click',()=>{
+    selectedArea.innerHTML = ``;
+    jobSearch.classList.add('job-search--hidden');
+    searchFilterList = [];
+})
+
+
+
+/* Remove One filter at a time */
+
+selectedArea.addEventListener('click',(e)=>{
+  let removingBadge;
+  if(e.target.classList.contains('remove1')) removingBadge = e.target.parentElement;
+  if(e.target.classList.contains('remove2')) removingBadge = e.target.parentElement.parentElement;
+  if(e.target.classList.contains('remove3')) removingBadge = e.target.parentElement.parentElement.parentElement;
+  if(removingBadge.parentNode){
+    removingBadge.parentNode.removeChild(removingBadge);
+  }
+  searchFilterList = [];
+  selectedArea.childNodes.forEach(badge=>{
+    searchFilterList.push(badge.textContent.trim());
+  });
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 
